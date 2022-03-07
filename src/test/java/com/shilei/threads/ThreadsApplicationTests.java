759ffe6960e0d9127eac175ee5aa3a68bf4d8351
shilei <leileishi97@gmail.com>
2022-03-07 22:22:13 +0800

    first commit

---
+++ src/test/java/com/shilei/threads/ThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shilei.threads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
